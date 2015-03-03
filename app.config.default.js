7be41b8cbc4dcab6410089265dbe66c025903888
Joe Critchley <joe@strobedigital.com>
2015-03-03 18:10:46 +0000

    Adding basic webpack stuff

---
+++ app.config.default.js
@@ -0,0 +1,6 @@
+module.exports = {
+	API_BASE: 'http://api.themoviedb.org/3',
+	API_KEY: 'f9ed33072b171189baab4e9e5c16a212',
+	API_USERNAME: 'joecritch',
+	API_PASSWORD: 'laracroft'
+};
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: __dirname + '/src/',
+    filename: 'bundle.js',
+    publicPath: '/src/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['react-hot', 'jsx?harmony'], exclude: /node_modules/ }
+    ]
+  }
+};
