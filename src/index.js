224e62c2c88ba4a08dc54f6aef27dc9572e2c7b6
Joe Critchley <joecritch@gmail.com>
2016-02-03 15:59:05 +0000

    Fixed merge conflict

--- src/index.js
+++ src/index.js
@@ -11,9 +11,4 @@ var host = document.getElementById('host');
 
 if(__DEV__) Perf.start();
 ReactDOM.render(React.createElement(AppContainer, {flux: flux}), host);
-<<<<<<< HEAD
-
-window.Perf = Perf;
-=======
 if(__DEV__) window.Perf = Perf;
->>>>>>> c9048c2... Added __DEV__ feature flag for webpack.config.js only
